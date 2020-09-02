9b049046e509ef9a1588fad1e40e7414c83b7fa4
Assdawut Homsub <assdawut.ho.61@ubu.ac.th>
2020-09-02 11:59:57 +0700

    Updates

---
+++ precache-manifest.672022ae1118cab1684fdf92e9c229e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4855ad1b90c3450bc31dcada5dc4b552",
+    "url": "/YansuoXo/index.html"
+  },
+  {
+    "revision": "09d2c58d1584c7674ad9",
+    "url": "/YansuoXo/static/css/main.b792a898.chunk.css"
+  },
+  {
+    "revision": "42791388e1b87353b0b0",
+    "url": "/YansuoXo/static/js/2.76e9d1fa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/YansuoXo/static/js/2.76e9d1fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09d2c58d1584c7674ad9",
+    "url": "/YansuoXo/static/js/main.b4fd6ded.chunk.js"
+  },
+  {
+    "revision": "5efeb9f0a9dff496f58c",
+    "url": "/YansuoXo/static/js/runtime-main.49df8693.js"
+  }
+]);
